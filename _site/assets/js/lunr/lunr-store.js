427070ca050e24cced205fe9b63c6dd8549ef34d
beomjo <qjatjr1108@gmail.com>
2021-09-27 00:49:04 +0900

    feat: BOJ 2193. 이친수

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -137,6 +137,12 @@ var store = [{
         "url": "/boj/boj-10844/",
         "teaser": "/assets/images/bio-photo.jpg"
       },{
+        "title": "BOJ 2193. 이친수",
+        "excerpt":"BOJ 2193.  이친수 풀이 마지막으로 오는 수가 0 일 때 그 앞에 올 수 있는 수는 0,1이다. 마지막으로 오는 수가 1 일 때 그 앞에 올 수 있는 수는 0이다. 점화식 d[i][0] = d[i-1][0] + d[i-1][1] d[i][1] = d[i-1][0] Bottom-Up class Main { public static void main(String[] args) { Scanner sc =...","categories": ["BOJ"],
+        "tags": ["Algorithm","BOJ"],
+        "url": "/boj/boj-2193/",
+        "teaser": "/assets/images/bio-photo.jpg"
+      },{
         "title": "Jekyll, minimal-mistakes 테마를 적용하기",
         "excerpt":"설정 Github Pages 호스팅을 위해 ${GITHUB_ID}.github.io 라는 이름으로 리파지토리를 생성해야 한다 Gem을 이용해서 Jekyll 을 설치한다 테마 적용 minimal-mistake 빠른 시작 가이드를 참고하여 진행한다. minimal-mistake Repository에서 소스를 가져와 옮긴다. 옮긴 후 다음 과정을 진행한다. 불필요 내용 제거 minimal-mistakes-jekyll repository에서 소스를 가져왔다면 불필요한 내용을 제거한다. .editorconfig .gitattributes .github /docs /test CHANGELOG.md...","categories": ["Blogs"],
         "tags": ["Jekyll","minimal-mistakes"],

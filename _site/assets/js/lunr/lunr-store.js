baa74e7a09df67028a9a0d3fedba29e1dc18d6f1
beomjo <qjatjr1108@gmail.com>
2021-09-27 00:55:34 +0900

    feat: BOJ 14002. 가장 긴 증가하는 부분수열 4(LIS)

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -149,6 +149,12 @@ var store = [{
         "url": "/boj/boj-2193/",
         "teaser": "/assets/images/bio-photo.jpg"
       },{
+        "title": "BOJ 14002. 가장 긴 증가하는 부분수열 4(LIS)",
+        "excerpt":"BOJ 14002. 가장 긴 증가하는 부분수열 4 (LIS) 풀이 beomseok95.tistory.com/355 위 문제와 같은 문제이지만 LIS의 길이뿐 아니라 모든 원소를 출력해야한다. 위 LIS 문제와 같이 푸는데, 역추적을 위한 배열을 만들어 LIS에 연결되는 인덱스를 저장한다. import java.io.IOException; import java.util.Scanner; import java.util.Stack; class Main { public static void main(String[] args) throws IOException {...","categories": ["BOJ"],
+        "tags": ["Algorithm","BOJ"],
+        "url": "/boj/boj-14002/",
+        "teaser": "/assets/images/bio-photo.jpg"
+      },{
         "title": "Jekyll, minimal-mistakes 테마를 적용하기",
         "excerpt":"설정 Github Pages 호스팅을 위해 ${GITHUB_ID}.github.io 라는 이름으로 리파지토리를 생성해야 한다 Gem을 이용해서 Jekyll 을 설치한다 테마 적용 minimal-mistake 빠른 시작 가이드를 참고하여 진행한다. minimal-mistake Repository에서 소스를 가져와 옮긴다. 옮긴 후 다음 과정을 진행한다. 불필요 내용 제거 minimal-mistakes-jekyll repository에서 소스를 가져왔다면 불필요한 내용을 제거한다. .editorconfig .gitattributes .github /docs /test CHANGELOG.md...","categories": ["Blogs"],
         "tags": ["Jekyll","minimal-mistakes"],

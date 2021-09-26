b0e8f3b36a51bd0f5c903ec47ce740310ced0e33
beomjo <qjatjr1108@gmail.com>
2021-09-27 01:10:30 +0900

    feat: BOJ 3085. 사탕게임

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -179,6 +179,12 @@ var store = [{
         "url": "/boj/boj-2309/",
         "teaser": "/assets/images/bio-photo.jpg"
       },{
+        "title": "BOJ 3085. 사탕게임",
+        "excerpt":"BOJ 3085. 사탕게임 풀이 NxN 크기의 보드에서 두 칸을 골라 서로 사탕을 교환하고 이때 인접해있는 사탕을 먹는다고 하였을 때 먹을 수 있는 사탕의 최대 개수를 구하는 방법은 보드 전체를 1번씩 확인해보아야 하는데, 아래와 오른쪽을 바꾸어가며 전체 보드를 확인해보면 보드를 모두 1번씩 확인할 수 있다. 이때 시간 복잡도는 O(NN)이다. 최대 주어지는 N의 수는...","categories": ["BOJ"],
+        "tags": ["Algorithm","BOJ"],
+        "url": "/boj/boj-3085/",
+        "teaser": "/assets/images/bio-photo.jpg"
+      },{
         "title": "Jekyll, minimal-mistakes 테마를 적용하기",
         "excerpt":"설정 Github Pages 호스팅을 위해 ${GITHUB_ID}.github.io 라는 이름으로 리파지토리를 생성해야 한다 Gem을 이용해서 Jekyll 을 설치한다 테마 적용 minimal-mistake 빠른 시작 가이드를 참고하여 진행한다. minimal-mistake Repository에서 소스를 가져와 옮긴다. 옮긴 후 다음 과정을 진행한다. 불필요 내용 제거 minimal-mistakes-jekyll repository에서 소스를 가져왔다면 불필요한 내용을 제거한다. .editorconfig .gitattributes .github /docs /test CHANGELOG.md...","categories": ["Blogs"],
         "tags": ["Jekyll","minimal-mistakes"],
